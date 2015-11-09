401e86cf122bdb9c8fa243ef749e2b2e4b221414
vijay <jvijayanand@gmail.com>
2015-11-09 15:05:41 -0500

    new 4th file added

---
+++ my_file4.java
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+package com.unifirst.platform.punchout.service;
+
+import com.unifirst.platform.punchout.exception.TransformationException;
+import com.unifirst.platform.punchout.message.incoming.MessageEntry;
+import com.unifirst.platform.punchout.message.transformed.ocr.OrderCreateResponseMessage;
+import com.unifirst.platform.punchout.message.transformed.ocr.OrderRequest;
+import com.unifirst.platform.punchout.message.transformed.poom.PunchOutOrderMessage;
+import com.unifirst.platform.punchout.message.transformed.posr.PunchInSetupRequestMessage;
+import com.unifirst.platform.punchout.message.transformed.posr.PunchInSetupResponseMessage;
+
+
+/**
+ * @author vijayanand Jambulingam
+ *
+ */
+public interface TransformationService
+{
+
+	PunchInSetupRequestMessage transformInboundPOSRMessage(MessageEntry entry) throws TransformationException;
+
+	String transformOutboundPOSRResponseMessage(PunchInSetupResponseMessage responseMessage, MessageEntry entry)
+			throws TransformationException;
+
+	String transformOutboundPOOM(final PunchOutOrderMessage poom) throws TransformationException;
+
+	OrderRequest transformInboundOCRMessage(MessageEntry entry) throws TransformationException;
+
+	String transformOutboundOCRResponseMessage(OrderCreateResponseMessage responseMessage, MessageEntry entry)
+			throws TransformationException;
+
+}
